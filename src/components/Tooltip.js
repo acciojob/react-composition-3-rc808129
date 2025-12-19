922f57763ed2dfecf5c7ab6c9a04551a7b5bec3c
rc808129 <rc808129@gmail.com>
2025-12-19 09:37:12 +0530

    tootltip complete

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -11,10 +11,10 @@ const Tooltip  = ({text, childern})=>{
   return(<div>
 
     { show && <div className="tooltiptext">{text}</div>}
-    <div className="tooltip" onMouseEnter={()=> setShow(true)} onMouseLeave={()=> setShow(false)}>{childern}</div>
+    <h2 className="tooltip" onMouseEnter={()=> setShow(true)} onMouseLeave={()=> setShow(false)}>{childern}</h2>
    {next && <div className="tooltiptext">{text}</div>}
-    <h1 
-    onMouseEnter={()=> setNext(true)} onMouseLeave={()=> setNext(false)}>Hover over me to see another tooltip</h1>
+    <p className="tooltip" 
+    onMouseEnter={()=> setNext(true)} onMouseLeave={()=> setNext(false)}>Hover over me to see another tooltip</p>
    
       
   </div>)
