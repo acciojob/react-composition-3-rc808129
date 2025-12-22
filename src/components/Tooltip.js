291aee902f4d0b191b62ae8c8898958baa293df8
rc808129 <rc808129@gmail.com>
2025-12-22 18:08:12 +0530

    c2

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -11,10 +11,20 @@ const Tooltip  = ({text, children})=>{
   return(<div>
 
     { show && <div className="tooltiptext">{text}</div>}
-    <h2 className="tooltip" onMouseEnter={()=> setShow(true)} onMouseLeave={()=> setShow(false)}>{children}</h2>
+    <h2 className="tooltip" onMouseEnter={()=> setShow(true)} onMouseLeave={()=> setShow(false)}>
+      
+      
+      <div>{children} </div>
+
+
+
+    </h2>
    {next && <div className="tooltiptext">{text}</div>}
     <p className="tooltip" 
-    onMouseEnter={()=> setNext(true)} onMouseLeave={()=> setNext(false)}>Hover over me to see another tooltip</p>
+    onMouseEnter={()=> setNext(true)} onMouseLeave={()=> setNext(false)}>
+     <div> Hover over me to see another tooltip </div>
+      
+      </p>
    
       
   </div>)
