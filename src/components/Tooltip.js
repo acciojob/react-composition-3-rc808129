635ac8907844bf109a799d172816ce44e28975e1
rc808129 <rc808129@gmail.com>
2025-12-23 06:55:20 +0530

    commit

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -4,13 +4,17 @@ const Tooltip = ({ text, children }) => {
   const [show, setShow] = useState(false);
 
   return (
+    <div>
     <div
       className="tooltip"
       onMouseEnter={() => setShow(true)}
       onMouseLeave={() => setShow(false)}
     >
+      <div>
       {children}
+      </div>
       {show && <span className="tooltiptext">{text}</span>}
+    </div>
     </div>
   );
 };
