f2df41bd03fd45ee556703d2b36c09d2488b48ac
rc808129 <rc808129@gmail.com>
2025-12-23 06:13:44 +0530

    commit time

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,43 +1,32 @@
-
 import React, { useState } from "react";
 
-
-const Tooltip  = ({text, children})=>{
-
+const Tooltip = ({ text, children }) => {
   const [show, setShow] = useState(false);
-  const [next, setNext] = useState(false)
+  const [next, setNext] = useState(false);
 
+  return (
+    <div>
+      {/* First Tooltip */}
+      <div
+        className="tooltip"
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
+        {children}
+        {show && <div className="tooltiptext">{text}</div>}
+      </div>
 
-  return(<div>
+      {/* Second Tooltip */}
+      <div
+        className="tooltip"
+        onMouseEnter={() => setNext(true)}
+        onMouseLeave={() => setNext(false)}
+      >
+        Hover over me to see another tooltip
+        {next && <div className="tooltiptext">This is a tooltip</div>}
+      </div>
+    </div>
+  );
+};
 
-    
-    <h2 className="tooltip" onMouseEnter={()=> setShow(true)} onMouseLeave={()=> setShow(false)}>
-      
-      
-      <div>{children}
-        
-          <div> 
-
-            { show && <div className="tooltiptext">{text}</div>}
-          </div>
-        
-         </div>
-
-
-
-    </h2>
-    <p className="tooltip" 
-    onMouseEnter={()=> setNext(true)} onMouseLeave={()=> setNext(false)}>
-     <div> Hover over me to see another tooltip 
-
-      <div>    {next && <div className="tooltiptext">This is a tooltip</div>}
-</div>
-     </div>
-      
-      </p>
-   
-      
-  </div>)
-}
-
-export default Tooltip
+export default Tooltip;
